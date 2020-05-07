f71a63f881d221bf7391c62c959ba7065bbaba28
Kris-Zheng <347111927@qq.com>
2020-05-07 13:39:48 +0800

    change in js

--- bullet-screen.js
+++ bullet-screen.js
@@ -23,24 +23,30 @@
             var r = Math.floor(Math.random() * 254);
             var g = Math.floor(Math.random() * 254);
             var b = Math.floor(Math.random() * 254);
-            var fontsize = Math.round(Math.random()*50);
-            odiv.css({                        //定义弹幕元素的随机样式（top位置,颜色）
+            
+            odiv.css({                        //定义弹幕元素的随机样式（top位置,颜色.字体大小）
                 color: "rgb(" + r + "," + g + "," + b + ")",
-                top: (Math.floor(Math.random() * container.height())-30) + "px",
-                width:odiv.width(),
-                fontSize: fontsize
+                top: (Math.floor(Math.random() * ((container.height()-60)-60)) + 60) + "px",
+                fontSize: (Math.round(Math.random()*(60-20)) +20) + "px",
+                width:odiv.width(),               
+                right: 0
             });
             container.append(odiv);
             this.move(odiv,container);
         },
     
         move:function(odiv,container){
-            var i = Math.floor(Math.random()*1920);;
+            var i = 0;
             var timer = setInterval(function() {
                 odiv.css({
-                    right: (i) + "px"
+                    right: (i += 1) + "px"
                 });
-            }, 0);
+                if ((odiv.offset().left + odiv.width()) < container.offset().left) {
+                    odiv.remove()
+                    clearInterval(timer)
+                }
+            }, 10);
+            this.timers.push(timer);
         },
         /*
          * 清除弹幕墙上的所有弹幕
